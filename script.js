b24478ced83dfbf430ade08dccd94200ae564910
Cookiewookie87 <jernej.tratnik@gmail.com>
2018-07-04 10:57:24 +0200

    Init version 3
    Updated code: added supress to fire events once, added more functions, fix for fast scroll to set panel stack on true, added disable mouse on scroll

--- script.js
+++ script.js
@@ -9,48 +9,109 @@
 
 const boxes = Array.from(document.querySelectorAll(".box"));
 const wrapper = document.querySelector(".wrapper");
-const body = document.querySelector("body");
 const dash = document.querySelector(".dashboard");
+const body = document.querySelector("body");
+const smallMargin = 20; // px
+const bigMargin = 60; // px
 const hoverMargin = 100; //px
+let isHover = false;
 let isScrolling; // flaf when user is scrolling
 let hoverExtendFlag = false; // flag to update if on hover panels are extended
 let boxesFlagArray = []; // flag for stacked panels
+let supressEvents = false;
+
 for(var i = 0; i < boxes.length; i++){
     boxesFlagArray.push({"isStacked": false});
+    let box = boxes[i];
+    box.addEventListener("click", panelClick)
+    box.addEventListener("mouseenter", onHover)
+    box.addEventListener("mouseleave", hoverLeave)
+}
+dash.addEventListener("mouseenter", dashMouseEnter);
+
+
+window.addEventListener("load", function(){
+    setBigMargin(bigMargin);
+});
+
+// function to slide panels to open (+)
+function slidingPanelsOpen(indexedElement) {
+    if (!isHover) { 
+        for (let i = indexedElement + 1; i < boxes.length; i++) {
+            const iCoord = boxes[i].getBoundingClientRect();
+            boxes[i].style.left = `${iCoord.left + hoverMargin}px`;
+        }
+    }
 }
 
-wrapper.addEventListener("scroll", function() {
+// function to slide panels to close (-)
+function slidingPanelsClose(indexedElement) {
+    if (isHover) { 
+        for (let i = indexedElement + 1; i < boxes.length; i++) {
+            const iCoord = boxes[i].getBoundingClientRect();
+            boxes[i].style.left = `${iCoord.left - hoverMargin}px`;
+        }
+    }
+}
 
+// function to set big margin on scroll panels
+function setBigMargin(bigMargin) { 
+    let multiply = 1;
+    boxes.forEach(box => {
+        box.style.left = `${bigMargin * multiply}px`;
+        multiply++;
+    });
+}
+// function to set small margin on scroll panels
+function setSmallMargin(smallMargin) { 
+    let multiply = 4; // dashboard is 60px width, next margin is 80px. 4*20 = 80
+    boxes.forEach((box, index) => {
+        if (index === 0) { 
+            box.style.left = `${bigMargin}px`; // first panel is allways 60px margin
+        } else if (index > 0) { 
+            box.style.left = `${smallMargin * multiply}px`; // all 1+ panel margin
+            multiply++;
+        }
+    });
+}
+
+// function to set scroll position
+function scrollPanel(scrollTo) { 
+    wrapper.scroll({
+        left: scrollTo,
+        behavior: "smooth"
+    });
+}
+
+function scrolling() {
     window.clearTimeout(isScrolling); // Clear our timeout throughout the scroll
-
+    //isHover = false;
     // when user is NOT scrolling:
+    supressEvents = true;
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].style.pointerEvents = "none";  // for disabling cursor hover or actions when scrolling
+    }
+
     isScrolling = setTimeout(function () {
         console.log("Scrolling has stopped.");
         for (let i = 0; i < boxes.length; i++) {
             boxes[i].style.pointerEvents = "auto";  // for enabling cursor hover or actions
         }
-        boxes.forEach(box => box.addEventListener("mouseenter", onHover));
-        boxes.forEach(box => box.addEventListener("mouseleave", onHoverLeave));
-        dash.addEventListener("mouseenter", dashMouseEnter);
-        boxes.forEach(box => box.addEventListener("click", panelClick));
-    }, 60);
+        supressEvents = false;
 
-    // when user is scrolling:
-    //console.log(wrapper.scrollLeft);
+    }, 200);
+    // console.log(wrapper.scrollLeft);
+
+    // when user IS scrolling
     boxes.forEach((box, index) => {
         const boxCoord = box.getBoundingClientRect();
         const dashCoord = dash.getBoundingClientRect();
         const dashRight = dashCoord.right;
         const leftSideOfCurrent = boxCoord.left; // coordinarion of left side of panel
         const rightSideOfCurrent = boxCoord.right; // coordinarion of right side of panel
-        const leftSideOfNextItem = (index < boxes.length - 1) ? box.nextElementSibling.getBoundingClientRect().left : 0; // coordinarion of left side of NEXT panel (when index is 8, the next sibling is 0 if it is less than 8 than it is next sibling)
+        const leftSideOfNextItem = box.nextElementSibling.getBoundingClientRect().left;
         const leftValue = parseInt(window.getComputedStyle(box, null).getPropertyValue("left")); // gets the left value of CSS without "px"
-        let multiplyShrinkMargin = 4; // when panels shrink we want first element to start stacking 20 * 4px (4 because we start at 4 panel)
-
-        for (let i = 0; i < boxes.length; i++) {
-            boxes[i].style.pointerEvents = "none";  // for disabling cursor hover or actions when scrolling
-        }
-
+        
         // change dashboard
         if (index === 0 && leftSideOfCurrent === leftValue) {
             //dash.style.backgroundImage = "url(img/0.1.png)";
@@ -84,22 +145,9 @@ wrapper.addEventListener("scroll", function() {
 
         // stacking controll
         if (index > 3 && leftSideOfCurrent <= leftValue) { 
-            boxes.forEach((box, index) => {
-                if (index > 0) {
-                    let shrinkMargin = 20;
-                    shrinkMargin *= multiplyShrinkMargin;
-                    box.style.left = shrinkMargin + "px";
-                    multiplyShrinkMargin++;   
-                }
-            });
+            setSmallMargin(smallMargin);
         } else if (index < 5 && leftSideOfCurrent > leftValue && !boxes[index].classList.contains("shadow")) { 
-            boxes.forEach((box, index) => {
-                if (index > -1) {
-                    let growMargin = 60;
-                    growMargin *= index + 1;
-                    box.style.left = growMargin + "px";
-                }
-            });
+            setBigMargin(bigMargin);
         }
 
         // setting flag (true/false) to stacked panels (reached leftValue)
@@ -113,69 +161,77 @@ wrapper.addEventListener("scroll", function() {
                 boxesFlagArray[index-1].isStacked = false;
             }
         }
+        if (wrapper.scrollLeft > 3570) { // fix when fast scroll to set panels stack to true
+            for(var i = 0; i < boxes.length - 1; i++){
+                boxesFlagArray[i].isStacked = true;
+            }
+        }
+
     });
+}
 
-}, false);
-
-
-function onHover(event) {
+function onHover(event) { 
+    if (supressEvents) return;
     const indexedElement = boxes.indexOf(this);
     const isPanelStacked = boxesFlagArray[indexedElement].isStacked;
     const fromBoxIndex = boxes.indexOf(event.fromElement);
     const toBoxIndex = boxes.indexOf(event.toElement);
     const boxCoordFrom = (fromBoxIndex >= 0) ? boxes[fromBoxIndex].getBoundingClientRect() : "";
     const boxCoordTo = boxes[toBoxIndex].getBoundingClientRect();
-
+    
     if (event.fromElement == null) return; // fix error: Cannot read property 'classList' of null
+
+    // controll if we hover from box to box and if panel is stacked
+    if (event.fromElement.classList.contains("box") && event.toElement.classList.contains("box") && isPanelStacked) {
+        if (!isHover) {
+            slidingPanelsOpen(indexedElement);
+            isHover = true;
+        }
+    }
 
     // controll if we hover from body to box and if panel is stacked and it is not expanded yet
     if((event.fromElement == body || event.fromElement == wrapper) && event.toElement.classList.contains("box") && isPanelStacked) {
         slidingPanelsOpen(indexedElement);
+        isHover = true;
     }
-    
-    // controll if we hover from box to box and if panel is stacked
-    if (event.fromElement.classList.contains("box") && event.toElement.classList.contains("box") && isPanelStacked) {
-        if (!hoverExtendFlag) {
-            slidingPanelsOpen(indexedElement);
-            hoverExtendFlag = true;
-        }
+
+    // From box to box hover controll
+    if (fromBoxIndex > toBoxIndex && isHover) {
+        event.fromElement.style.left = `${boxCoordFrom.left + hoverMargin}px`;
+    } else if ((fromBoxIndex < toBoxIndex && fromBoxIndex > -1) && isHover) {
+        event.toElement.style.left = `${boxCoordTo.left - hoverMargin}px`;
     }
 
     // controll mouse from dashboard to box
     if (event.toElement.classList.contains("box") && event.fromElement === dash && isPanelStacked) { 
         slidingPanelsOpen(indexedElement);
-        hoverExtendFlag = true;
-    }
-
-    // From box to box hover controll
-    if (fromBoxIndex > toBoxIndex && hoverExtendFlag) {
-        event.fromElement.style.left = `${boxCoordFrom.left + hoverMargin}px`;
-    } else if ((fromBoxIndex < toBoxIndex && fromBoxIndex > -1) && hoverExtendFlag) {
-        event.toElement.style.left = `${boxCoordTo.left - hoverMargin}px`;
+        isHover = true;
     }
 }
 
-function onHoverLeave(event) {
+function hoverLeave() { 
+    if (supressEvents) return;
     const indexedElement = boxes.indexOf(this);
     const isPanelStacked = boxesFlagArray[indexedElement].isStacked;
     const toBoxIndex = boxes.indexOf(event.toElement);
-    
-    if (event.toElement == null) return; // fix error: Cannot read property 'classList' of null
 
+    if (event.toElement == null) return; // fix error: Cannot read property 'classList' of null
+    
     // controll the mouse from box to body when left margin is narrow or extended
     if((event.toElement == body || event.toElement == wrapper) && event.fromElement.classList.contains("box")) {
         slidingPanelsClose(indexedElement);
-        hoverExtendFlag = false;
+        isHover = false;
     }
 
     // controll from box to box hover out, to return to initial state
     if (event.fromElement.classList.contains("box") && event.toElement.classList.contains("box") && (isPanelStacked === false || (isPanelStacked === true && boxesFlagArray[toBoxIndex].isStacked === false ))) { // checks if the from is true and to is false (first false element)
         slidingPanelsClose(indexedElement);
-        hoverExtendFlag = false;
+        isHover = false;
     }
 }
 
 function dashMouseEnter(event) { 
+    if (supressEvents) return;
     if (event.fromElement == null) return; // fix error: Cannot read property 'classList' of null
 
     // controll hover on dashboard from box
@@ -184,11 +240,12 @@ function dashMouseEnter(event) {
             const iCoord = boxes[i].getBoundingClientRect();
             boxes[i].style.left = `${iCoord.left - hoverMargin}px`;
         }
-        hoverExtendFlag = false;
+        isHover = false;
     }
 }
 
 function panelClick() {
+    if (supressEvents) return;
     const indexedElement = boxes.indexOf(this);
     const isPanelStacked = boxesFlagArray[indexedElement].isStacked;
     //TODO: this must be done in more dynamic way
@@ -200,6 +257,7 @@ function panelClick() {
             break;
         case 1:
             scrollPanel(607);
+
             break;
         case 2:
             scrollPanel(1059);
@@ -216,33 +274,8 @@ function panelClick() {
         default:
             break;
         }
+        isHover = false;
     }
 }
 
-// function to slide panels to open (+)
-function slidingPanelsOpen(indexedElement) { 
-    // TODO hover still fires when panel 4 is clicked
-    for (let i = indexedElement + 1; i < boxes.length; i++) {
-        const iCoord = boxes[i].getBoundingClientRect();
-        boxes[i].style.left = `${iCoord.left + hoverMargin}px`;
-    }
-}
-// function to slide panels to close (-)
-function slidingPanelsClose(indexedElement) {
-    for (let i = indexedElement + 1; i < boxes.length; i++) {
-        const iCoord = boxes[i].getBoundingClientRect();
-        boxes[i].style.left = `${iCoord.left - hoverMargin}px`;
-    }
-}
-// function to set scroll position
-function scrollPanel(scrollTo) { 
-    wrapper.scroll({
-        left: scrollTo,
-        behavior: "smooth"
-    });
-}
-
-
-
-
-
+wrapper.addEventListener("scroll", scrolling);
